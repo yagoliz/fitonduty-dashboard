717a61e121d5b3e7ff194f9c5bf8ada00907fa21
Yago Lizarribar <yago.lizarribar@gmail.com>
2025-05-15 13:31:12 +0200

    remove unnecessary imports

--- main.py
+++ main.py
@@ -4,14 +4,6 @@ import index
 # Import callbacks
 from callbacks import admin_callbacks, participant_callbacks
 
-# Import components
-import components.admin.sidebar
-import components.admin.group_comparison
-import components.admin.group_summary
-import components.admin.participant_detail
-import components.participant.date_selector
-import components.participant.health_metrics
-import components.participant.detailed_charts
 
 # Start the server
 if __name__ == '__main__':
--- wsgi.py
+++ wsgi.py
@@ -6,14 +6,6 @@ import index
 # Import all callbacks to ensure they're registered
 from callbacks import admin_callbacks, participant_callbacks
 
-# Import components that might register callbacks
-import components.admin.sidebar
-import components.admin.group_comparison
-import components.admin.group_summary
-import components.admin.participant_detail
-import components.participant.date_selector
-import components.participant.health_metrics
-import components.participant.detailed_charts
 
 # Now the application is fully initialized and can be served by Gunicorn
 if __name__ == "__main__":
